1e225e8a207594ee25c9ee292f224c8b384e1a1b
Anecka7L <165366413+Anecka7L@users.noreply.github.com>
2026-03-16 22:34:11 +0500

    Add files via upload

---
+++ security.permaviat.ru/ajax/login_user.php
@@ -0,0 +1,53 @@
+<?php
+	session_start();
+	include("../settings/connect_datebase.php");
+	
+	$login = $_POST['login'];
+	$password = $_POST['password'];
+
+	function decryptAES($encryptedData, $key) {
+		$data = base64_decode($encryptedData);
+
+		if ($data === false || strlen($data) < 17) {
+			error_log("Invalid data or too short");
+			return false;
+		}
+
+		$iv = substr($data, 0, 16);
+		$encrypted = substr($data, 16);
+
+		$keyHash = md5($key);
+		$keyHash = hex2bin($keyHash);
+
+		$decrypted = openssl_decrypt(
+			$encrypted,
+			'aes-128-cbc',
+			$keyBytes,
+			OPENSSL_RAW_DATA,
+			$iv
+		);
+
+		return $decrypted;
+	}
+
+	$login_encrypted = $_POST['login'];
+	$password_encrypted = $_POST['password'];
+
+	$secretKey = "qazxswedcvfrtgbn";
+
+	$login = decryptAES($login_encrypted, $secretKey);
+	$password = decryptAES($password_encrypted, $secretKey);
+	
+	// ищем пользователя
+	$query_user = $mysqli->query("SELECT * FROM `users` WHERE `login`='".$login."' AND `password`= '".$password."';");
+	
+	$id = -1;
+	while($user_read = $query_user->fetch_row()) {
+		$id = $user_read[0];
+	}
+	
+	if($id != -1) {
+		$_SESSION['user'] = $id;
+	}
+	echo md5(md5($id));
+?>

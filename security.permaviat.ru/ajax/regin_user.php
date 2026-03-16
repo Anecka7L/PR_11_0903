3d49fcea83ee5f2d4d9e05f699895c8472b810eb
Anecka7L <165366413+Anecka7L@users.noreply.github.com>
2026-03-17 02:12:12 +0500

    Update regin_user.php

--- security.permaviat.ru/ajax/regin_user.php
+++ security.permaviat.ru/ajax/regin_user.php
@@ -2,8 +2,44 @@
 	session_start();
 	include("../settings/connect_datebase.php");
 	
-	$login = $_POST['login'];
-	$password = $_POST['password'];
+	function decryptAES($encryptedData, $key) {
+		$data = base64_decode($encryptedData);
+
+		if ($data === false || strlen($data) < 17) {
+			error_log("Invalid data or too short");
+			return false;
+		}
+
+		$iv = substr($data, 0, 16);
+		$encrypted = substr($data, 16);
+
+		$keyHash = md5($key);
+		$keyBytes = hex2bin($keyHash);
+
+		$decrypted = openssl_decrypt(
+			$encrypted,
+			'aes-128-cbc',
+			$keyBytes,
+			OPENSSL_RAW_DATA,
+			$iv
+		);
+
+		return $decrypted;
+	}
+
+	$login_encrypted = $_POST['login'];
+	$password_encrypted = $_POST['password'];
+
+	$secretKey = "qazxswedcvfrtgbn";
+
+	$login = decryptAES($login_encrypted, $secretKey);
+	$password = decryptAES($password_encrypted, $secretKey);
+	
+
+	if (!$login || !$password) {
+		echo -1;
+		exit();
+	}
 	
 	// ищем пользователя
 	$query_user = $mysqli->query("SELECT * FROM `users` WHERE `login`='".$login."'");
@@ -12,9 +48,11 @@
 	if($user_read = $query_user->fetch_row()) {
 		echo $id;
 	} else {
-		$mysqli->query("INSERT INTO `users`(`login`, `password`, `roll`) VALUES ('".$login."', '".$password."', 0)");
+		// Хешируем пароль перед сохранением в БД
+		$hashed_password = md5($password);
+		$mysqli->query("INSERT INTO `users`(`login`, `password`, `roll`) VALUES ('".$login."', '".$hashed_password."', 0)");
 		
-		$query_user = $mysqli->query("SELECT * FROM `users` WHERE `login`='".$login."' AND `password`= '".$password."';");
+		$query_user = $mysqli->query("SELECT * FROM `users` WHERE `login`='".$login."' AND `password`= '".$hashed_password."';");
 		$user_new = $query_user->fetch_row();
 		$id = $user_new[0];
 			
